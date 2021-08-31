9264e4510cf3fe83dece7291acabb8d7a780b2fc
Shahariar Abdullah Isha <shahariaresha@gmail.com>
2021-08-31 11:23:24 +0600

    router dom add

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,12 +1,12 @@
 import React from "react";
+import { BrowserRouter as Router, Route } from "react-router-dom";
 import Temp from "./Components/weather/temp";
-
 
 function App() {
   return (
-    <>
-    <Temp />
-    </>
+    <Router>
+      <Route path="/" component={Temp} exact />
+    </Router>
   );
 }
 
--- frontend/src/Components/weather/temp.js
+++ frontend/src/Components/weather/temp.js
@@ -1,94 +1,72 @@
-//https://api.openweathermap.org/data/2.5/weather?q=dhaka&appid=57dfc0f457b007c70c0b30e226e0a228
-import React, {useState, useEffect} from 'react'
-import "./style.css"
+import React, { useState, useEffect } from "react";
+import WeatherCard from "./weatherCard";
+import "./style.css";
 
 const Temp = () => {
-    const [searchValue,setSearchValue] = useState("Dhaka");
+  const [searchValue, setSearchValue] = useState("Dhaka");
+  const [tempInfo, setTempInfo] = useState({});
 
-    const getWeatherInfo = async () => {
-        try{
-            let url =
-            `https://api.openweathermap.org/data/2.5/weather?q=${searchValue}&
-            appid=57dfc0f457b007c70c0b30e226e0a228`;
+  const getWeatherInfo = async () => {
+    try {
+      let url = `https://api.openweathermap.org/data/2.5/weather?q=${searchValue}&units=metric&appid=57dfc0f457b007c70c0b30e226e0a228`;
 
-            const res = fetch(url);
-            const data
-        } catch(error){
-            console.log(error);
-        }
-    };
-    useEffect(() => {
-        getWeatherInfo();
-    }, [])
-    return (
-        <>
-            <div className="wrap">
-                <div className="search">
-                    <input type="search" 
-                    placeholder="search..." 
-                    autoFocus 
-                    id="search" 
-                    className="searchTerm"
-                    value={searchValue}
-                    onChange={(e) => setSearchValue(e.target.value)}
-                    />
-                    <button className="searchButton" type="button" onClick={getWeatherInfo}>
-                        Search
-                    </button>
-                </div>
-            </div>
+      let res = await fetch(url);
+      let data = await res.json();
 
-            <article className="widget">
-                <div className="weatherIcon">
-                <i className={"wi wi-day-sunny"}></i>
-                </div>
-                <div className="weatherInfo">
-                    <div className="temperature">
-                        <span>25.5&deg;</span>
-                    </div>
-                    <div className="description">
-                        <div className="weatherCondition">sunny</div>
-                        <div className="place">Dhaka, Bangladesh</div>
-                    </div>
-                </div>
-                <div className="date">{new Date().toLocaleString()}</div>
+      const { temp, humidity, pressure } = data.main;
+      const { main: weathermood } = data.weather[0];
+      const { name } = data;
+      const { speed } = data.wind;
+      const { country, sunset } = data.sys;
 
-                <div className="extra-temp">
-                    <div className="temp-info-minmax">
-                        <div className="two-sided-section">
-                            <p>
-                                <i className={"wi wi-sunset"}></i>
-                            </p>
-                            <p className="extra-info-leftside">
-                            19:19 pm <br/>
-                            sunset
-                            </p>
-                        </div>
+      const myNewWeatherInfo = {
+        temp,
+        humidity,
+        pressure,
+        weathermood,
+        name,
+        speed,
+        country,
+        sunset,
+      };
 
-                        <div className="two-sided-section">
-                            <p>
-                                <i className={"wi wi-rain"}></i>
-                            </p>
-                            <p className="extra-info-leftside">
-                            19:19 pm <br/>
-                            sunset
-                            </p>
-                        </div>
+      setTempInfo(myNewWeatherInfo);
+    } catch (error) {
+      console.log(error);
+    }
+  };
 
-                        <div className="two-sided-section">
-                            <p>
-                                <i className={"wi wi-rain"}></i>
-                            </p>
-                            <p className="extra-info-leftside">
-                            19:19 pm <br/>
-                            sunset
-                            </p>
-                        </div>
-                    </div>
-                </div>
-            </article>
-        </>
-    )
-}
+  useEffect(() => {
+    getWeatherInfo();
+  }, []);
 
-export default Temp
+  return (
+    <>
+      <div className="wrap">
+        <div className="search">
+          <input
+            type="search"
+            placeholder="search..."
+            autoFocus
+            id="search"
+            className="searchTerm"
+            value={searchValue}
+            onChange={(e) => setSearchValue(e.target.value)}
+          />
+
+          <button
+            className="searchButton"
+            type="button"
+            onClick={getWeatherInfo}>
+            Search
+          </button>
+        </div>
+      </div>
+
+      {/* our temp card  */}
+      <WeatherCard {...tempInfo} />
+    </>
+  );
+};
+
+export default Temp;
